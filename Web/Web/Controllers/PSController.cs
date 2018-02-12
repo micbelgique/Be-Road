8e42bb8bad1cf56ac5bb09b0033abe6861b68d9f
MDA06 <michaelvm171@msn.com>
2018-02-12 13:01:43 +0100

    Added the public services to the a view

--- Web/Web/Controllers/PSController.cs
+++ Web/Web/Controllers/PSController.cs
@@ -3,23 +3,31 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Web.Dal;
 using Web.Models;
 
 namespace Web.Controllers
 {
     public class PSController : Controller
     {
+        private PSContext context = new PSContext();
+
         public ActionResult Index()
         {
             Session["connected"] = true;
             EidCard eid = (EidCard)Session["eid"];
-            return View("Index", eid);
+            if (eid != null)
+                ViewBag.Name = $"{eid.FirstName} {eid.MiddleName} {eid.LastName}";
+            else
+                ViewBag.Name = "Incognito";
+            //return View("Index", eid);
+            return View("Index", context.PublicServices.ToList());
         }
 
         [HttpGet]
-        public ActionResult Select(string _id)
+        public ActionResult Select(string id)
         {
-            return RedirectToAction("Index", "PSData", new { id = _id });
+            return RedirectToAction("Index", "PSData", new { id = id });
         }
     }
 }
