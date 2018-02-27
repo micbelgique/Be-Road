8acf027427a06dd7dfca3c6f6db09b658195fb77
MDA06 <michaelvm171@msn.com>
2018-02-27 15:37:45 +0100

    Added ServiceController to display user information

---
+++ Web/CopsService/Controllers/ServiceController.cs
@@ -0,0 +1,35 @@
+﻿using PublicService.Dal;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PublicService.Controllers
+{
+    [Authorize]
+    public class ServiceController : Controller
+    {
+        private PSContext db = new PSContext();
+
+        [HttpGet]
+        public ActionResult Index()
+        {
+            var users = db.Users.ToList();
+            return View(users);
+        }
+
+        public ActionResult UploadToAzure()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(String name)
+        {
+            throw new NotImplementedException("Search users by name is not yet implemented");
+        }
+
+
+    }
+}
