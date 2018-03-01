05543bddb9d11188986a53d1016477eb13a57654
Wilson Weets <wilson.weets@mic-belgique.be>
2018-03-01 12:37:55 +0100

    Added Swagger-Net NuGet package

--- Web/CopsService/Global.asax.cs
+++ Web/CopsService/Global.asax.cs
@@ -1,9 +1,11 @@
 ﻿using PublicService.Dal;
+using Swagger.Net.Application;
 using System;
 using System.Collections.Generic;
 using System.Data.Entity;
 using System.Linq;
 using System.Web;
+using System.Web.Http;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
