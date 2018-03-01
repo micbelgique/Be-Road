ba72a2a04d60ba7610188d225fbf666775a4a6bf
Wilson Weets <wilson.weets@mic-belgique.be>
2018-03-01 10:13:18 +0100

    - Added azure upload when register and manage

--- Web/CopsService/Controllers/AccountController.cs
+++ Web/CopsService/Controllers/AccountController.cs
@@ -222,6 +222,7 @@ namespace PublicService.Controllers
                     var result = await UserManager.CreateAsync(user, model.Password);
                     if (result.Succeeded)
                     {
+                        au.UploadToAzure(db);
                         return await Login(new LoginViewModel() { Username = model.Username, Password = model.Password }, "/Account/Manage");
                     }
                     AddErrors(result);
@@ -268,6 +269,7 @@ namespace PublicService.Controllers
             user.ExtraInfo = new Data() { Value = postUser.ExtraInfo };
             await UserManager.UpdateAsync(user);
             db.SaveChanges();
+            au.UploadToAzure(db);
             return RedirectToAction("Index", "Home");
         }
 
