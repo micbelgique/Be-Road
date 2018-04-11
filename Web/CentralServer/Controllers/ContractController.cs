3fe831c03cec150133b3395dbae48803bd80137e
Wilson Weets <wilson.weets@mic-belgique.be>
2018-04-11 17:01:41 +0200

    Wip : Contrats creation (inputs) Making querries, mappings and ouputs

---
+++ Web/CentralServer/Controllers/ContractController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CentralServer.Controllers
+{
+    public class ContractController : Controller
+    {
+        // GET: Contract
+        public ActionResult Index()
+        {
+            return View("Create");
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+    }
+}
---
+++ Web/CentralServer/Scripts/Script.js
@@ -0,0 +1,14 @@
+﻿$(function () {
+    var inputNb = 0;
+    $('.input-add-btn').click(
+        function () {
+            inputNb++;
+            $('#input').clone().appendTo('#inputs').attr('id', inputNb);
+            $('#' + inputNb).show();
+            $('#' + inputNb).find('.input-rem-btn').bind('click', function () {
+                var id = this.parentElement.parentElement.id;
+                $('#inputs').find('#' + id).remove();
+            });
+        }
+    );
+});
