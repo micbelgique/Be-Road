f7f164395076eeedcb0853f94b4b3c2d26ebfb65
Wilson Weets <wilson.weets@mic-belgique.be>
2018-04-30 10:04:45 +0200

    Log model creation

---
+++ Web/MessageLog/Models/Log.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MessageLog.Models
+{
+    public enum Determiner
+    {
+        BeContract,
+        AdapterServer
+    }
+
+    public class Log
+    {
+        public int Id { get; set; }
+        public Determiner Deter { get; set; }
+        public string ContractId { get; set; }
+        public bool Response { get; set; }
+        public string UserType { get; set; }
+        public string UserName { get; set; }
+        public DateTime CreationDate { get; set; }
+        public AccessInfo Access { get; set; }
+    }
+}
