a3e18e78c6f625fdaabbda85290de382b91882e4
Wilson Weets <wilson.weets@mic-belgique.be>
2018-04-30 11:43:48 +0200

    Log model : added Message

--- Web/MessageLog/Models/Log.cs
+++ Web/MessageLog/Models/Log.cs
@@ -21,6 +21,7 @@ namespace MessageLog.Models
         public bool Response { get; set; }
         public string UserType { get; set; }
         public string UserName { get; set; }
+        public string Message { get; set; }
         public DateTime CreationDate { get; set; }
     }
 }
